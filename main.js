b64c445247641b1a6949ea1f5eb18a0c6c0e750e
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-11-03 11:45:31 -0500

    Add date-time line

--- main.js
+++ main.js
@@ -51,3 +51,26 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
   const id = parseInt(idString, 10);
   store.removeBook(id);
 });
+
+// Add date
+
+function refreshTime() {
+  const timeDisplay = document.getElementById('date-time');
+  const dateOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  };
+  const dateString = new Date().toLocaleString('en-us', dateOptions);
+  timeDisplay.textContent = dateString;
+}
+
+function initTime() {
+  refreshTime();
+  setInterval(refreshTime, 1000);
+}
+
+initTime();
