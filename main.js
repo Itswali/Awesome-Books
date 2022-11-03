be4e1a8884121450fd0546bb1e57ff7712aa258b
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-11-02 19:32:04 -0500

    Clean comments lines

--- main.js
+++ main.js
@@ -1,50 +1,6 @@
 import Book from './js/book.js';
 import Store from './js/store.js';
 
-/*
-class Book {
-  constructor(title, author, id) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-  }
-}
-
-class Store {
-  constructor() {
-    this.count = this.getBooks().length + 1;
-  }
-
-  getBooks() {
-    if (localStorage.getItem('books') === null) {
-      this.books = [];
-    } else {
-      this.books = JSON.parse(localStorage.getItem('books'));
-    }
-    return this.books;
-  }
-
-  addBook(book) {
-    const newBook = {
-      id: this.count,
-      title: book.title,
-      author: book.author,
-    };
-    const books = this.getBooks();
-    books.push(newBook);
-    localStorage.setItem('books', JSON.stringify(books));
-    this.count += 1;
-  }
-
-  removeBook(id) {
-    const books = this.getBooks();
-    const filteredBooks = books.filter((book) => book.id !== id);
-    localStorage.setItem('books', JSON.stringify(filteredBooks));
-  }
-}
-*/
-
-
 const store = new Store();
 class UI {
   static displayBooks() {
