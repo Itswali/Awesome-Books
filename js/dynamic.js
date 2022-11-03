7681ad56e30162e44177bfed09688483390a9645
Wali-Dexter <luckywali0986@gmail.com>
2022-11-03 20:28:10 +0500

    fix the events listner

--- js/dynamic.js
+++ js/dynamic.js
@@ -8,12 +8,15 @@ const contactshow = document.getElementById('contactshow');
 
 showlist.addEventListener('click' , () => {
     list.style.display = "flex";
+    add.style.display = "none";
+    contact.style.display = "none";
 
 } );
 
 addshow.addEventListener('click' , () => {
     list.style.display = "none";
     add.style.display = "flex";
+    contact.style.display = "none";
 
 } );
 
